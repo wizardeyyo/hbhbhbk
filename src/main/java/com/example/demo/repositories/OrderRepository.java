d39a9f44900e8df410b203c06b5f64202581e0e4
ApAcheGod <31448839+ApAcheGod@users.noreply.github.com>
2022-06-03 23:15:30 +0500

    orders by this week

--- src/main/java/com/example/demo/repositories/OrderRepository.java
+++ src/main/java/com/example/demo/repositories/OrderRepository.java
@@ -4,6 +4,7 @@ import com.example.demo.entities.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Repository
@@ -11,4 +12,5 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
 
     List<Order> findAllByStatus(String status);
 
+    List<Order> findAllByShippingDateBetween(LocalDate monday, LocalDate sunday);
 }
